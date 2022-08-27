d5ff2c080d1e7a2d1d4f52b6ca0890444131dfb5
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-27 18:51:25 -0400

    update

--- src/app/listado/listado.page.ts
+++ src/app/listado/listado.page.ts
@@ -6,21 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado.page.scss'],
 })
 export class ListadoPage implements OnInit {
-  listado : [
-    { id : '1',
-      nombre : 'Juan',
-      direccion : 'Americo Vespucio 1501'
-    },
-    { id : '2',
-      nombre : 'Ana',
-      direccion : 'Americo Vespucio 2501'
-    },
-    { id : '3',
-      nombre : 'Leo',
-      direccion : 'Americo Vespucio 3501'
-    }
-  ];
-  constructor() { }
+  listado : any;
+    
+  constructor() { 
+    this.listado = [{ id : '1',
+                nombre : 'Juan',
+                direccion : 'Americo Vespucio 1501'
+              },
+              { id : '2',
+                nombre : 'Ana',
+                direccion : 'Americo Vespucio 2501'
+              },
+              { id : '3',
+                nombre : 'Leo',
+                direccion : 'Americo Vespucio 3501'
+              }];
+
+  }
 
   ngOnInit() {
   }
