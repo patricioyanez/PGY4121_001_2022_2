0cbad6bbf7616df47e9d7cc69db318137b632cad
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-27 18:36:07 -0400

    update

---
+++ src/app/listado/listado.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.page.html',
+  styleUrls: ['./listado.page.scss'],
+})
+export class ListadoPage implements OnInit {
+  listado : [
+    { id : '1',
+      nombre : 'Juan',
+      direccion : 'Americo Vespucio 1501'
+    },
+    { id : '2',
+      nombre : 'Ana',
+      direccion : 'Americo Vespucio 2501'
+    },
+    { id : '3',
+      nombre : 'Leo',
+      direccion : 'Americo Vespucio 3501'
+    }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
